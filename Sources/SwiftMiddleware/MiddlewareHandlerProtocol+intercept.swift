ce48f963b1b65e6c0f09eea04b2ccabd9c373510
Simon Pilkington <simonpi@amazon.com>
2022-08-02 12:17:18 +1000

    Remove EmptyMiddlewarePhase.

---
+++ Sources/SwiftMiddleware/MiddlewareHandlerProtocol+intercept.swift
@@ -0,0 +1,22 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the swift-middleware open source project
+//
+// Copyright (c) swift-middleware project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of swift-middleware project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+import Foundation
+
+public extension MiddlewareHandlerProtocol {
+    func intercept<MiddlewareType: MiddlewareProtocol>(middleware: MiddlewareType) -> some MiddlewarePhaseProtocol
+    where MiddlewareType.InputType == InputType, MiddlewareType.OutputType == OutputType {        
+        return MiddlewarePhase(next: self, with: middleware)
+    }
+}
