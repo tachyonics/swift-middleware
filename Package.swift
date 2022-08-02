f03d174a390bb73315876cb5ae417872dff4622b
Simon Pilkington <simonpi@amazon.com>
2022-08-02 11:12:43 +1000

    Fix target name.

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "swift-middleware",
-            targets: ["swift-middleware"]),
+            name: "SwiftMiddleware",
+            targets: ["SwiftMiddleware"]),
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
@@ -21,7 +21,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "swift-middleware", dependencies: [
+            name: "SwiftMiddleware", dependencies: [
                 .product(name: "Logging", package: "swift-log"),
             ]),
     ],
